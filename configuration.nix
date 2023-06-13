ebeed44ce00944b58a37f27ac9aebb62f357c23d
Solly Ross <solly.ross@fastly.com>
2023-06-13 16:26:18 -0700

    [boot] use systemd initrd for fancy pw prompt
    systemd initrd allows plymouth to be in charge of the password, which
    just looks nicer

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
 			enable = true;
 			theme = "breeze";
 		};
+		initrd.systemd.enable = true; # pretty password prompt
 
 		# TODO(directxman12): try out `initrd.systemd.enable` for password prompt in plymouth
 		# (see https://github.com/NixOS/nixpkgs/issues/26722)
