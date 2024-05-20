fba5d552bc70a667668117d0a4f27b365e751e5c
Solly Ross <directxman12+gh@gmail.com>
2024-05-19 17:41:28 -0700

    Add steam
    includes both main steam (which doesn't quite work), and various command
    line tools for launch games (which do work).

--- common-system-apps.nix
+++ common-system-apps.nix
@@ -5,6 +5,10 @@
 		"google-chrome-dev"
 		"google-chrome"
 		"google-chrome-beta"
+		"steam"
+		"steam-original"
+		"steam-run"
+		"steamcmd"
 	];
 	environment.systemPackages = with pkgs; [
 		# consider using the first-party flake in neovim/neovim for nightly once it builds again
@@ -12,7 +16,19 @@
 		bat
 		fd
 		ripgrep
-		unstable.google-chrome-dev
+		# temporary, till it's in nixos, otherwise we get sigsegvs on google drive
+		((unstable.google-chrome.overrideAttrs (final: prev: {
+			name = "google-chrome-dev-${final.version}";
+			version = "124.0.6356.2";
+			src = pkgs.fetchurl {
+				url = "https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-unstable/google-chrome-unstable_${final.version}-1_amd64.deb";
+				hash = "sha256-4245YF7Jet3n2bcNtovha27C0YW6QyvDz4r/M+Nsiuw=";
+			};
+			# nacl_helper was removed?
+			installPhase = builtins.replaceStrings [",nacl_helper"] [""] prev.installPhase;
+		})).override {
+			channel = "dev";
+		})
 
 		# needed for flakes when git is present
 		git
