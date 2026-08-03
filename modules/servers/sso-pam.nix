3fc4a1d3f21631053a9551d0e391d2a387f0d884
Solly Ross <git@metamagical.dev>
2026-08-02 17:06:14 -0700

    [flake] update to 26.05

--- modules/servers/sso-pam.nix
+++ modules/servers/sso-pam.nix
@@ -3,7 +3,7 @@
 with lib; {
 	metamagical.sso.pam = mkIf config.local.server.enable {
 		enable = true;
-		config-version = mkIf (!config.local.unstable) "2-ish";
+		config-version = "2";
 		server = mkDefault "https://sso.metamagical.house";
 	};
 
