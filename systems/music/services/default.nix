44a6c96c91512b4f56ab7379bc25040b11a3c4e7
Solly Ross <git@metamagical.dev>
2026-09-06 00:42:50 -0700

    [music] use plex from unstable
    it'll be a smidge behind, but still way more recent

--- systems/music/services/default.nix
+++ systems/music/services/default.nix
@@ -17,13 +17,7 @@
 	services.plex = {
 		enable = true;
 		openFirewall = true;
-		package = pkgs.plex.overrideAttrs (final: old: {
-			version = "1.41.0.8994-f2c27da23";
-			src = pkgs.fetchurl {
-				url = "https://downloads.plex.tv/plex-media-server-new/${final.version}/debian/plexmediaserver_${final.version}_amd64.deb";
-				sha256 = "sha256-e1COeawdR0pCF+qQ/xkTn/716iM9kB/fXom5MWHQ0YI=";
-			};
-		});
+		package = pkgs.unstable.plex;
 	};
 
 	services.unifi = {
